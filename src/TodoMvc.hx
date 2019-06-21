dd9f24e0ea98b0e9fcd804b5b79de6e355015a32
Juraj Kirchheim <back2dos@googlemail.com>
2019-06-21 19:52:16 +0200

    Begin work on haxeui implementation.

--- src/TodoMvc.hx
+++ src/TodoMvc.hx
@@ -2,7 +2,7 @@ package ;
 
 import js.Browser.*;
 import coconut.Ui.hxx;
-import todomvc.ui.TodoListView;
+// import todomvc.ui.TodoListView;
 
 class TodoMvc {
   static function main() {
@@ -12,9 +12,22 @@ class TodoMvc {
     for (i in 0...Std.parseInt(window.location.hash.substr(1)))
       list.add('item $i');
 
+    #if haxeui_core
+		// var root = new Component();
+		// Screen.instance.addComponent(root);
+		// root.width = 500;
+		// root.height = 500;
+		// coconut.ui.Renderer.mount(
+		// 	root,
+		// 	coconut.Ui.hxx('<MyView />')
+		// );
+		
+		// Toolkit.init();
+    #else
     coconut.ui.Renderer.mount(
       cast document.body.appendChild(document.createDivElement()),
       hxx('<TodoListView todos=${list}/>')
     );
+    #end
   }
 }
--- src/todomvc/ui/TodoItemView.hx
+++ src/todomvc/ui/TodoItemView.hx
@@ -11,25 +11,21 @@ class TodoItemView extends View {
   @:attribute var ondeleted:TodoItem->Void;
   @:state var isEditing:Bool = false;
 
-  function render() {
-
-    function edit(entered:String)
-      switch entered.rtrim() {
-        case '': ondeleted(item);
-        case v: item.description = v;
-      }
-
-    return @hxx '
-      <li class="todo-item" data-completed=${item.completed} data-editing={js.Lib.undefined}>
-        <input name="completed" type="checkbox" checked=${item.completed} onchange={item.completed = event.src.checked} />
-        <if ${isEditing}>
-          <input ref=${function (i) if (i != null) i.focus()} name="description" type="text" value=${item.description} onchange={edit(event.src.value)} onblur={isEditing = false} />
-        <else>
-          <span class="description" ondblclick=${this.isEditing = true}>{item.description}</span>
-          <button class="delete" onclick=${ondeleted(item)}>Delete</button>
-        </if>
-      </li>
-    ';
-  }
+  function edit(entered:String)
+    switch entered.rtrim() {
+      case '': ondeleted(item);
+      case v: item.description = v;
+    }
+  function render() '
+    <li class="todo-item" data-completed=${item.completed} data-editing={js.Lib.undefined}>
+      <input name="completed" type="checkbox" checked=${item.completed} onchange={item.completed = event.src.checked} />
+      <if ${isEditing}>
+        <input ref=${function (i) if (i != null) i.focus()} name="description" type="text" value=${item.description} onchange={edit(event.src.value)} onblur={isEditing = false} />
+      <else>
+        <span class="description" ondblclick=${this.isEditing = true}>{item.description}</span>
+        <button class="delete" onclick=${ondeleted(item)}>Delete</button>
+      </if>
+    </li>
+  ';
 
 }
