8fc58bfb2437bd9a9dcd278ad907b9fb174d3c69
Juraj Kirchheim <back2dos@googlemail.com>
2020-07-05 20:29:18 +0200

    Add hotswap.

--- src/TodoMvc.hx
+++ src/TodoMvc.hx
@@ -9,7 +9,7 @@ import haxe.ui.core.*;
 
 class TodoMvc {
   static function main() {
-    
+
     var list = new todomvc.data.TodoList();
 
     for (i in 0...Std.parseInt(window.location.hash.substr(1)))
@@ -24,13 +24,17 @@ class TodoMvc {
 			root,
 			hxx('<todomvc.haxeui.TodoListView todos=${list} />')
 		);
-		
+
 		haxe.ui.Toolkit.init();
     #else
     coconut.ui.Renderer.mount(
       cast document.body.appendChild(document.createDivElement()),
       hxx('<todomvc.ui.TodoListView todos=${list}/>')
     );
+    #end
+
+    #if hotswap
+      hotswap.Runtime.permaPoll();
     #end
   }
 }
